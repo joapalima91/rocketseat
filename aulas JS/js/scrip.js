cd7048ccf436c11a250033e21d0f28df25856180
João Paulo da SIlva lima <joaopalima91@gmail.com>
2022-08-29 21:42:51 -0300

    declaração JS
    declarando variáveis em JS

--- aulas JS/js/scrip.js
+++ aulas JS/js/scrip.js
@@ -1,6 +1,30 @@
-console.log("Bem vindo ao Starter")
 
-var array = ["João Paulo", 31, "Vascaíno"]
+// declarando um object
+let student = {
+    name: "João",
+    age: 30,
+    weight: 78.7,
+    isSubscribed: true,
+}
+
+console.log(student)
+console.log(`${student.name} de idade ${student.age} pesa  ${student.weight} kg.`)
+
+//declarando array students
+
+let students = []
+
+students = [
+    student
+]
+
+console.log(students)
+
+// coloque no console o valor da posição dde zero do array a cima
+console.log(students[0])
+
+
+
 
 
 
