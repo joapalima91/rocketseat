7d43e8193c1db8ea413d95c62bd590c404d2302c
João Paulo da Silva Lima <joaopalima91@gmail.com>
2022-08-30 16:09:05 -0300

    Update script02.js
    scripts

--- aulas JS/js/script02.js
+++ aulas JS/js/script02.js
@@ -1,8 +1,30 @@
 // Manipulando arrays
-
 let techs = ["html", "css", "js"]
 
 // adicionar um item no fim
+//techs.push("nodejs")
+//console.log(techs)
 
-techs.push("nodejs")
+// adicionar um item no inicio do array
+//techs.unshift("sql")
+//console.log(techs)
+
+// remover do fim
+//techs.pop()
+//console.log(techs)
+
+
+// remover do inicio
+//techs.shift()
+//console.log(techs)
+
+
+// remover 1 ou mais elementos em qualquer posição do array
+//techs.splice(1,2)
 console.log(techs)
+
+//encontrar posição
+
+let index = techs.indexOf('css')
+
+console.log(index)
